091b118542c3b1a6e721e4e251f858680f769370
jelias00c <jesus.elias@me.com>
2017-06-19 16:16:29 -0700

    first commit

---
+++ uninstall_smart_notebook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# remove smart notebook
+
+LICENSE_KEY=ENTER_KEY
+SMART_NB=/Applications/SMART\ Technologies/Notebook.app
+
+if [[ -e ${SMART_NB} ]]; then
+  echo "App installed. Attempting to recall SMART license..."
+  /Library/Application\ Support/SMART\ Technologies/activationwizard.app/Contents/MacOS/activationwizard --puid education_bundle --m=4 --v=3 --return --pk $LICENSE_KEY
+  echo "Attempting to remove SMART Tools..."
+  /Applications/SMART\ Technologies/SMART\ Uninstaller.app/Contents/Resources/uninstall --all
+fi
+
+exit 0
