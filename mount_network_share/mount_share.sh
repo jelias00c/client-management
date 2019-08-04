38f4e252e58bbbc49c803fd34ac42fcec0aec1ad
Jesus Elias <jelias@navi.local>
2019-08-03 20:10:09 -0700

    updated script
    now written completely in bash. uses a single applescript command to mount the share.

--- mount_network_share/mount_share.sh
+++ mount_network_share/mount_share.sh
@@ -1,28 +1,51 @@
-#!/bin/sh
-mount_script=`/usr/bin/osascript > /dev/null << EOF
-set mountShare to true
+#!/bin/bash
+# login script
 
-# check if share is already mounted
-tell application "Finder"
-	delay 1.0
-	set disklist to list disks
-	repeat with i from 1 to number of items in disklist
-		if item i of disklist is equal to "DriveShareName" then
-			set mountShare to false
-		end if
-	end repeat
-end tell
+mount_drive=0
+shareName="Shared_Files"
+serverName="File_Server"
+domainName="AD_DOMAIN"
+log_file="~/Library/Logs/mount_drives.log"
 
-# check if server is reachable
-set pingStatus to (do shell script "ping -c 1 server.domain.pri; echo offline")
-if pingStatus is equal to "offline" then
-	set mountShare to false
-end if
+# function for log file time stamp
+get_timestamp () {
+	timestamp=$(/bin/date '+%Y-%m-%d %H:%M:%S')
+}
 
-# mount share
-if mountShare is true then
-	ignoring application responses
-		mount volume "smb://server.domain.pri/DriveShareName"
-	end ignoring
-end if
-EOF`
+sleep 4
+
+# check for domain account lockout
+useruid=$(/usr/bin/dscl . -read /Users/$USER UniqueID | /usr/bin/awk '{print $2}')
+if [[ $useruid -gt '1000' ]]; then
+	userLockout=$(/usr/bin/dscl /Active\ Directory/$domainName/All\ Domains -read /Users/$USER | /usr/bin/grep "dsAttrTypeNative:lockoutTime:" | /usr/bin/awk '{print $2}')
+	if [[ $userLockout -ne '0' ]]; then
+		lockoutTime=$((($userLockout/10000000)-11644473600))
+		converted_date=$(/bin/date -r $lockoutTime '+%Y-%m-%d %I:%M:%S %p')
+		get_timestamp; /bin/echo "$timestamp ERROR: Domain account is locked. Locked on $converted_date." >> $log_file
+		exit 1
+	fi
+fi
+
+# check if item already mounted
+OLDIFS=$IFS; IFS=$'\n'
+for i in $(/bin/ls /Volumes/); do
+	if [[ $i == $shareName ]]; then
+		get_timestamp; /bin/echo "$timestamp ALERT: $shareName volume already mounted." >> $log_file
+		mount_drive=1
+	fi
+done
+IFS=$OLDIFS
+
+# check if servers are reachable
+testPing=$(/sbin/ping -c 1 $serverName | /usr/bin/grep -i packets | /usr/bin/awk '{print $4}')
+if [[ $testPing -ne '1' ]]; then
+	get_timestamp; /bin/echo "$timestamp ALERT: $shareName server unreachable." >> $log_file
+	mount_drive=1
+fi
+
+# attempt mount shares
+if [[ $mount_drive -eq '0' ]]; then
+	/usr/bin/osascript -e 'mount volume "smb://$serverName/$shareName"'
+fi
+
+exit 0
